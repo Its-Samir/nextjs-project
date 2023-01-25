a53f2441a6331037e98541cdf362e90b1dd12798
samir <shubhod2k18@gmail.com>
2023-01-25 23:58:29 +0530

    navbar updated

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,13 +1,15 @@
 import classes from './MainNavigation.module.css';
 import Link from 'next/link';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { useState } from 'react';
 
 function MainNavigation() {
-
+  const [showMenu, setShowMenu] = useState(false);
+  
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
-      <nav>
-        <ul>
+        <ul className={`${classes.List} ${showMenu && classes.ShowMenu}`}>
           <li>
             <Link href='/'>All Meetups</Link>
           </li>
@@ -15,7 +17,7 @@ function MainNavigation() {
             <Link href='/new-meetup'>Add New Meetup</Link>
           </li>
         </ul>
-      </nav>
+      <RxHamburgerMenu onClick={() => setShowMenu((prevValue) => !prevValue)} className={classes.MenuIcon} />
     </header>
   );
 }
--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -3,18 +3,21 @@ import classes from './MeetupList.module.css';
 
 function MeetupList(props) {
   return (
-    <ul className={classes.list}>      
-      {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-          onLoad={props.onLoad}
-        />
-      ))}
-    </ul>
+    <>
+      <h1>The MeetUps</h1>
+      <ul className={classes.list}>
+        {props.meetups.map((meetup) => (
+          <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            onLoad={props.onLoad}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
 
